b7743c8478d5a9a147b5a943b31e3ce392f51c4e
Isabela de Aro <euisabeladearo@gmail.com>
2022-07-22 18:59:06 -0300

    refactor: change data

--- index.js
+++ index.js
@@ -17,12 +17,13 @@ const generateStocksJson = async () => {
   const stocks = [];
 
   $(".table-container > table > tbody > tr").map((_, row) => {
-    const codAtivo = $(row).find("td:nth-child(1)").text();
+    const ticker = $(row).find("td:nth-child(1)").text();
     const nome = $(row).find("td:nth-child(2)").text();
     const cotacao = $(row).find("td:nth-child(3)").text();
 
     stocks.push({
-      codAtivo,
+      codAtivo: stocks.length + 1,
+      ticker,
       nome,
       cotacao: Number(cotacao.split("R$ ")[1].replace(/,/g, ".")),
       moeda: "BRL",
@@ -35,6 +36,7 @@ const generateStocksJson = async () => {
 
   const dbData = stocks.map((stock) => ({
     codAtivo: stock.codAtivo,
+    ticker: stock.ticker,
     qtdeAtivo: Math.floor(Math.random() * (10000 - 5000)) + 5000,
   }));
 
